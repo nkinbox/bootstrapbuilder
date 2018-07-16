6c5f4f14675e82252f90fa9953a26d6cb9763208
N Kumar <nkinbox@outlook.com>
2018-07-16 16:32:55 +0530

    Deleting Locality Name

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -15,10 +15,18 @@ class Location extends Model
     public function geoLocation() {
         return $this->hasOne('App\Models\GeoLocation', 'id', 'geolocation_id');
     }
+    public function hotels() {
+        return $this->hasMany('App\Models\Hotel');
+    }
+    public function packages() {
+        return $this->hasMany('App\Models\PackageItinerary');
+    }
     protected static function boot() {
         parent::boot();        
         static::deleting(function($model) {
             $model->getContent()->delete();
+            $model->hotels()->update(["location_id" => 0]);
+            $model->packages()->update(["location_id" => 0]);
         });
     }
 }
