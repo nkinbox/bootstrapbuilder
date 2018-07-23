15ccc9379cbbbc17b06e1a8e1f487cb9385504d9
N Kumar <nkinbox@outlook.com>
2018-07-23 13:22:29 +0530

    Query Builder

--- resources/views/Website/html.blade.php
+++ resources/views/Website/html.blade.php
@@ -14,7 +14,7 @@
             return $propertyResolver($match_[1], $loops);
         }, json_encode($element->var_attributes));
         try {
-            dd("\$element_var_attributes=".$element_var_attributes.";");
+            eval("\$element_var_attributes=".$element_var_attributes.";");
         } catch (ParseError $e) {}
         $var_attributes = json_decode($element_var_attributes, true);
     } else {
