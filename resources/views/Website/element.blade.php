58f9bfd968821d38b51234981268491df072ef86
N Kumar <nkinbox@outlook.com>
2018-07-27 15:12:54 +0530

    Added Loops to Source Code

--- resources/views/Website/element.blade.php
+++ resources/views/Website/element.blade.php
@@ -141,8 +141,6 @@
                         }, $object_query);
                     }
                     try {
-                        if($ls->id == 8)
-                        dd("\$query = ".$object_query.";");
                         eval("\$query = ".$object_query.";");
                         $loops[$loop_count]['loaded'][$db_var->object] = eval("return \App\Models\\".$db_var->object."::".(($query)?$query:"all()").";");
                     } catch (ParseError $e) {
