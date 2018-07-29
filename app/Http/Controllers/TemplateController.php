ce47ec17800c2c4a887400b2c6171992b77a81ab
N Kumar <nkinbox@outlook.com>
2018-07-29 15:24:05 +0530

    Added PHP evaluation in Template

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -84,15 +84,14 @@ class TemplateController extends Controller
                         if($db_var->property) {
                             if($isSet) {
                                 $eval .= "->".$db_var->property;
-                                // if(!$db_var->is_array && $db_var->related_to) {
-                                if($db_var->related_to) {
+                                if(!$db_var->is_array && $db_var->related_to) {
                                     $hasOneRelation = $db_var->related_to;
                                 }
                             }
                         }
                     } else {
                         $eval .= "->".$db_var->property;
-                        if($db_var->related_to) {
+                        if(!$db_var->is_array && $db_var->related_to) {
                             $hasOneRelation = $db_var->related_to;
                         }
                     }
